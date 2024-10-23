c840271c62c30986d8519936f63fb71f438140e2
Daniel Burrows <108002994+stackneo@users.noreply.github.com>
2024-10-23 15:01:23 +0100

    Allow algorithm to buy and sell

--- FXapi.py
+++ FXapi.py
@@ -23,35 +23,30 @@ def trade(trader_id, qty, side):
     res = requests.post(api_url, json=data)
     if res.status_code == 200:
         resp_json = json.loads(res.content.decode('utf-8'))
-        print(res.content.decode('utf-8'))
-        prices = []
-        for i in range(qty):
-            price = resp_json["price"]
-            prices.append(price)
-        #EMA formula, takes last value minus first value divided by n+1. Add first value.
-        #TODO: Make it less sensitive.
-        ema = (float(prices[-1]) - float(prices[0]) / (1 + 1)) + float(prices[0])
-        return ema
-    return None
+        trading_algorithm(qty, resp_json, trader_id)
 
+def trading_algorithm(qty, resp_json, trader_id):
+    prices = [resp_json["price"]]
+    ema = prices[0]
+    ema_period = 2
+    alpha = 2/(ema_period+1)
+    for i in range(1, qty):
+        #TODO: Get new prices instead of constantly grabbing same ones
+        current_price = resp_json["price"]
+        prices.append(current_price)
+        ema = float(ema) + alpha * (float(current_price)-float(ema))
 
-def trade2(trader_id, qty, side):
-    api_url = URL + "/trade/EURGBP"
-    data = {"trader_id": trader_id, "quantity": qty, "side": side}
-    res = requests.post(api_url, json=data)
-    if res.status_code == 200:
-        resp_json = json.loads(res.content.decode('utf-8'))
-        print(res.content.decode('utf-8'))
+        #Buy when low
+        if float(current_price) > float(ema):
+            print(f"Buying at {current_price}")
+            trade(trader_id, i, Side.BUY)
 
-        price = resp_json["price"]
-        #EMA formula, takes last value minus first value divided by n+1. Add first value.
-        #TODO: Make it less sensitive.
-        return price
-    return None
-
-
+        #Sell when high
+        elif float(current_price) < float(ema):
+            print(f"Selling at {current_price}")
+            trade(trader_id, i, Side.SELL)
 
 
 if __name__ == '__main__':
     print("Expected to trade at:" + str(get_price()))
-    print("Effectively traded at:" + str(trade2(TRADER_ID, 1, Side.BUY)))
+    print("Effectively traded at:" + str(trade(TRADER_ID, 100, Side.BUY)))
